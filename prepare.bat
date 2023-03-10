bf3ef8329ee953043307b67f809b49d014931d1a
polarfuchs23 <73428431+polarfuchs23@users.noreply.github.com>
2023-03-10 14:15:36 +0100

    Update prepare.bat

--- prepare.bat
+++ prepare.bat
@@ -2,6 +2,10 @@
 pip install -r requirements.txt
 echo pip installs done...
 echo. {}>dm_channels.json
+cd. >subscription1.csv
+cd. >subscription2.csv
+cd. >subscription3.csv
+cd. >subscription4.csv
 set /p TOKEN="Discord token: "
 echo TOKEN=%TOKEN%>.env
 pause
