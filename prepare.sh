cf27b199f06f5ff97bbe55cb02cc55f291f6cd31
polarfuchs23 <73428431+polarfuchs23@users.noreply.github.com>
2023-03-10 14:13:39 +0100

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,10 @@
 
 pip install -r requirements.txt
 touch dm_channels.json
+touch subscription1.csv
+touch subscription2.csv
+touch subscription3.csv
+touch subscription4.csv
 read -p "Discord token: " TOKEN
 touch .env
 echo "TOKEN=$TOKEN">.env
