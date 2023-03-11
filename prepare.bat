c67360dc686fb30a22e8188e489fe242bd72ab18
polarfuchs23 <73428431+polarfuchs23@users.noreply.github.com>
2023-03-11 16:43:57 +0100

    Changed pip install

--- prepare.bat
+++ prepare.bat
@@ -1,5 +1,5 @@
 @echo off
-pip install -r requirements.txt
+python -m pip install -r requirements.txt || python3 -m pip install -r requirements.txt
 echo pip installs done...
 echo. {}>dm_channels.json
 cd. >subscription1.csv
